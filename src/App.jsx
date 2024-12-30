8479139b34272eadcbf3c547b772a38c76b2d896
SPS <sumitpalsingh007@gmail.com>
2024-12-30 20:36:44 +0530

    adding search button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
 
 const title = 'React';
+const welcome = {
+    greeting: 'Hey',
+    title: 'React',
+};
+
+function getTitle(title) {
+    return title;
+}
 
 function App() {
     return (
         <div>
-            <h1>Hello React</h1>
+            <h1>Hello {getTitle('React')}</h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
         </div>
     );
 }
