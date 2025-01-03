8526c51fa63b4a3d128e2d9cdb6e1c4abf3e92db
SPS <sumitpalsingh007@gmail.com>
2025-01-03 21:12:25 +0530

    using React Reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,19 @@ const getAsyncStories = () =>
         )
     );
 
+const storiesReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_STORIES':
+            return action.payload;
+        case 'REMOVE_STORY':
+            return state.filter(
+                (story) => action.payload.objectID !== story.objectID
+            );
+        default:
+            throw new Error();
+    }
+};
+
 const initialStories = [
     {
         title: 'React',
@@ -47,7 +60,10 @@ const App = () => {
         'React'
     );
 
-    const [stories, setStories] = React.useState([]);
+    const [stories, dispatchStories] = React.useReducer(
+        storiesReducer,
+        []
+    );
     const [isLoading, setIsLoading] = React.useState(false);
     const [isError, setIsError] = React.useState(false);
 
